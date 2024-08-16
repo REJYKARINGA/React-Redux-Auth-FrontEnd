375631fcf70ca1877a6663df4cb233de0e1c98ec
REJYKARINGA <kp.rejy1972@gmail.com>
2024-08-17 02:48:12 +0530

    create sign up upi route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from '../api/routes/user.route.js';
+import authRoutes from '../api/routes/auth.route.js';
+
 
 dotenv.config() 
 mongoose.connect(process.env.MONGO_DB)
@@ -16,3 +18,4 @@ app.listen(3000,()=>{
 
 
 app.use('/api/user',userRoutes)
+app.use('api/auth',authRoutes)
