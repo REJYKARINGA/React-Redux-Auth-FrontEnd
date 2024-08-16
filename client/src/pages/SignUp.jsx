1c5b9f6b1d68f68b56ab86b5ff8e08d4090713de
REJYKARINGA <kp.rejy1972@gmail.com>
2024-08-17 03:53:11 +0530

    complete  sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -11,7 +11,7 @@ const SignUp = () => {
       <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
     <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
-      <div className='flex'>
+      <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
         <Link to='/signin' >
         <span className='text-blue-500'>Sign In</span></Link>
